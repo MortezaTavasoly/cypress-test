c32ad893d21166281a050fe9fca641524cde338d
MortezaTavasoly <vector.1475225963@gmail.com>
2023-12-30 16:37:39 -0800

    first-commit

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,16 @@
-// https://docs.cypress.io/api/table-of-contents
-
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
-  })
-})
+describe("navifation buttons", () => {
+  it("navigating from home to about", () => {
+    cy.visit("http://localhost:8080/");
+    cy.contains("button", "Go To About Page From Here");
+    cy.get('[data-test="about-button"]').click();
+    cy.location("pathname").should("equal", "/about");
+    cy.contains("h1", "This the about page");
+  });
+  it("navigating from about to home", () => {
+    cy.visit("http://localhost:8080/about");
+    cy.contains("h1", "This the about page");
+    cy.get('[data-test="home-button"]').click();
+    cy.location("pathname").should("equal", "/");
+    cy.contains("button", "Go To About Page From Here");
+  });
+});
